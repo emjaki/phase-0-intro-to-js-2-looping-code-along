e658ab093ad50932407838840cab214259885c2b
Emily <emilykiddell@gmail.com>
2022-03-07 17:19:28 +1100

    completed assignment (attempt 1)

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+guests = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(guests, event) {
+    const messages = [];
+    for (let i=0; i < guests.length; i++) {
+           const message = `Thank you, ${guests[i]}, for the wonderful ${event} gift!\n`;
+           messages.push(message);
+        }
+        return messages;  
+    }
+
+let count;
+
+function countDown(count) {
+    while (count > -1) {
+        console.log(count);
+        count--;
+    }
+}
+
+
+
+
+
